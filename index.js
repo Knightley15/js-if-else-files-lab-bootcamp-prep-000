1738592545b5978cfb0a16942ca53525230b1111
Juliette Knightley <knightleyjb@gmail.com>
2019-02-10 18:24:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ if (name === "Joe") { console.log ("The name is correct")}
 if (name === "Susan") { console.log ("incorrect")}
 
 var height = "74"
-if (height === 70) { console.log ("The height is correct")}
+if (height === 74) { console.log ("The height is correct")}
+if (height) === 70 { console.log (incorrect)}
+
 
 
 // Don't worry about this
