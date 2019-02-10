a4b408dabe89c30d986d8aa3ba16f9736c1606f8
Juliette Knightley <knightleyjb@gmail.com>
2019-02-10 18:25:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ if (name === "Susan") { console.log ("incorrect")}
 
 var height = "74"
 if (height === 74) { console.log ("The height is correct")}
-if (height) === 70 { console.log (incorrect)}
+if (height) === 70 { console.log ("incorrect")}
 
 
 
